90427b42e5da830311ce1684d23569c1565ff5ec
gracecummings <graceelainecummings@gmail.com>
2021-09-23 12:18:52 +0200

    updated binning

--- runSelectionsToPlots.py
+++ runSelectionsToPlots.py
@@ -6,7 +6,7 @@ if __name__=='__main__':
     #steps to run
     #assumes you have run the whole thing at the start of the day
     #steps = {"selections":True,"uncs":True,"ratios":True,"opts":True}
-    steps = {"topiary":True,"selections":False,"uncs":False,"ratios":False,"opts":False,"cutflow":False}
+    steps = {"topiary":False,"selections":True,"uncs":False,"ratios":False,"opts":False,"cutflow":False}
     
     #cut list, Zpt, Hpt, met,btagger,btagwp
     cutlist = [['150.0','300.0','200.0','DeepMassDecorrelTagZHbbvsQCD','0.8'],
@@ -22,97 +22,97 @@ if __name__=='__main__':
     plots = ['h_z_pt']#['h_h_pt','h_z_pt','h_met','h_nd_jigm','h_zp_jigm','h_h_sd','h_btag']
 
     #topiary sample list: dateforfolder, samplename
-    samplelist = [#['2021-06-17','Run2017B-31Mar2018-v1.SingleMuon'],
-                  #['2021-06-17','Run2017C-31Mar2018-v1.SingleMuon'],
-                  #['2021-06-17','Run2017D-31Mar2018-v1.SingleMuon'],
-                  #['2021-06-17','Run2017E-31Mar2018-v1.SingleMuon'],
-                  #['2021-06-17','Run2017F-31Mar2018-v1.SingleMuon'],
+    samplelist = [['2021-09-09','Run2017B-31Mar2018-v1.SingleMuon'],
+                  ['2021-09-09','Run2017C-31Mar2018-v1.SingleMuon'],
+                  ['2021-09-09','Run2017D-31Mar2018-v1.SingleMuon'],
+                  ['2021-09-09','Run2017E-31Mar2018-v1.SingleMuon'],
+                  ['2021-09-09','Run2017F-31Mar2018-v1.SingleMuon'],
                   #['2021-06-17','ZpAnomalonHZ_UFO-Zp1200-ND175-NS1'],
                   #['2021-06-17','ZpAnomalonHZ_UFO-Zp2000-ND300-NS1'],
                   #['2021-06-17','ZpAnomalonHZ_UFO-Zp2000-ND500-NS200'],
                   #['2021-06-17','ZpAnomalonHZ_UFO-Zp2000-ND800-NS200'],
                   #['2021-06-17','ZpAnomalonHZ_UFO-Zp3000-ND1200-NS1'],
-                  #['2021-06-17','Fall17.TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_new_pmx'],
-                  ['2021-06-17','Fall17.TTToHadronic_TuneCP5_13TeV-powheg-pythia8_new_pmx'],##ttbar bg
-                  ['2021-06-17','Fall17.TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_new_pmx'],##ttbar bg
-                  #['2021-06-17','Fall17.WZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8'],
-                  #['2021-06-17','Fall17.ZZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8'],
-                  #['2021-06-17','Fall17.DYJetsToLL_M-50_HT-100to200_TuneCP5_13TeV-madgraphMLM-pythia8'],
-                  #['2021-06-17','Fall17.DYJetsToLL_M-50_HT-200to400_TuneCP5_13TeV-madgraphMLM-pythia8'],
-                  #['2021-06-17','Fall17.DYJetsToLL_M-50_HT-400to600_TuneCP5_13TeV-madgraphMLM-pythia8'],
-                  #['2021-06-17','Fall17.DYJetsToLL_M-50_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8'],
-                  #['2021-06-17','Fall17.DYJetsToLL_M-50_HT-800to1200_TuneCP5_13TeV-madgraphMLM-pythia8'],
-                  #['2021-06-17','Fall17.DYJetsToLL_M-50_HT-1200to2500_TuneCP5_13TeV-madgraphMLM-pythia8'],
-                  #['2021-06-17','Fall17.DYJetsToLL_M-50_HT-2500toInf_TuneCP5_13TeV-madgraphMLM-pythia8'],
-                  #['2021-06-17','Run2018C-17Sep2018-v1.SingleMuon'],
-                  #['2021-06-17','Run2018B-17Sep2018-v1.SingleMuon'],
-                  #['2021-06-17','Run2018A-17Sep2018-v1.SingleMuon'],
-                  ['2021-06-17','Run2018D-22Jan2019-v2.SingleMuon'],#ttbar background
-                  #['2021-06-17','Autumn18.TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8'],##ttbar bg
-                  #['2021-06-17','Autumn18.TTToHadronic_TuneCP5_13TeV-powheg-pythia8'],##ttbar bg
-                  #['2021-06-17','Autumn18.TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8'],##ttbar bg
-                  #['2021-06-17','Autumn18.DYJetsToLL_M-50_HT-100to200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
-                  #['2021-06-17','Autumn18.DYJetsToLL_M-50_HT-200to400_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
-                  #['2021-06-17','Autumn18.DYJetsToLL_M-50_HT-400to600_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
-                  #['2021-06-17','Autumn18.DYJetsToLL_M-50_HT-600to800_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
-                 # ['2021-06-17','Autumn18.DYJetsToLL_M-50_HT-800to1200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
-                  #['2021-06-17','Autumn18.DYJetsToLL_M-50_HT-1200to2500_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
-                  #['2021-06-17','Autumn18.DYJetsToLL_M-50_HT-2500toInf_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp1200-ND200-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp1300-ND400-NS1_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-09','Fall17.TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_new_pmx'],
+                  ['2021-09-09','Fall17.TTToHadronic_TuneCP5_13TeV-powheg-pythia8_new_pmx'],##ttbar bg
+                  ['2021-09-09','Fall17.TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_new_pmx'],##ttbar bg
+                  ['2021-09-09','Fall17.WZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8'],
+                  ['2021-09-09','Fall17.ZZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8'],
+                  ['2021-09-09','Fall17.DYJetsToLL_M-50_HT-100to200_TuneCP5_13TeV-madgraphMLM-pythia8'],
+                  ['2021-09-09','Fall17.DYJetsToLL_M-50_HT-200to400_TuneCP5_13TeV-madgraphMLM-pythia8'],
+                  ['2021-09-09','Fall17.DYJetsToLL_M-50_HT-400to600_TuneCP5_13TeV-madgraphMLM-pythia8'],
+                  ['2021-09-09','Fall17.DYJetsToLL_M-50_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8'],
+                  ['2021-09-09','Fall17.DYJetsToLL_M-50_HT-800to1200_TuneCP5_13TeV-madgraphMLM-pythia8'],
+                  ['2021-09-09','Fall17.DYJetsToLL_M-50_HT-1200to2500_TuneCP5_13TeV-madgraphMLM-pythia8'],
+                  ['2021-09-09','Fall17.DYJetsToLL_M-50_HT-2500toInf_TuneCP5_13TeV-madgraphMLM-pythia8'],
+                  ['2021-09-09','Run2018C-17Sep2018-v1.SingleMuon'],
+                  ['2021-09-09','Run2018B-17Sep2018-v1.SingleMuon'],
+                  ['2021-09-09','Run2018A-17Sep2018-v1.SingleMuon'],
+                  ['2021-09-09','Run2018D-22Jan2019-v2.SingleMuon'],#ttbar background
+                  ['2021-09-08','Autumn18.TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8'],##ttbar bg
+                  ['2021-09-08','Autumn18.TTToHadronic_TuneCP5_13TeV-powheg-pythia8'],##ttbar bg
+                  ['2021-09-08','Autumn18.TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8'],##ttbar bg
+                  ['2021-09-08','Autumn18.DYJetsToLL_M-50_HT-100to200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
+                  ['2021-09-08','Autumn18.DYJetsToLL_M-50_HT-200to400_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
+                  ['2021-09-08','Autumn18.DYJetsToLL_M-50_HT-400to600_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
+                  ['2021-09-08','Autumn18.DYJetsToLL_M-50_HT-600to800_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
+                  ['2021-09-08','Autumn18.DYJetsToLL_M-50_HT-800to1200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
+                  ['2021-09-08','Autumn18.DYJetsToLL_M-50_HT-1200to2500_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
+                  ['2021-09-08','Autumn18.DYJetsToLL_M-50_HT-2500toInf_TuneCP5_PSweights_13TeV-madgraphMLM-pythia'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp1200-ND200-NS1_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp1300-ND400-NS1_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp1500-ND200-NS1_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp1500-ND600-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp1500-ND600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp1500-ND600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2000-ND400-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2000-ND400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2000-ND400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2000-ND800-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2000-ND800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2000-ND800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2500-ND1000-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2500-ND1000-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2500-ND1000-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2500-ND200-NS1_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2500-ND600-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2500-ND600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp2500-ND600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3000-ND1200-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3000-ND1200-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3000-ND1200-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3000-ND400-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3000-ND400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3000-ND400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3000-ND800-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3000-ND800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3000-ND800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3500-ND1000-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3500-ND1000-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3500-ND1000-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3500-ND1400-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3500-ND1400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3500-ND1400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3500-ND200-NS1_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3500-ND600-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3500-ND600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp3500-ND600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND1200-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND1200-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND1200-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND1600-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND1600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND1600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND400-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND800-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4000-ND800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND1000-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND1000-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND1000-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND1400-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND1400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND1800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND1400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND1800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND200-NS1_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND600-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp4500-ND600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND1200-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND1200-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND1200-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND1600-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND1600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND1600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND200-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND600-NS1_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5500-ND1000-NS200_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5500-ND1400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5500-ND1800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
-                  #['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5500-ND600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5000-ND800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5500-ND1000-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5500-ND1400-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5500-ND1800-NS200_TuneCP5_13TeV-madgraph-pythia8'],
+                  ['2021-09-08','Autumn18.ZpAnomalonHZ_UFO-Zp5500-ND600-NS200_TuneCP5_13TeV-madgraph-pythia8'],
                   ]
 
     if steps["topiary"]:
@@ -131,7 +131,7 @@ if __name__=='__main__':
             for samp in samplelist:
                 subprocess.run(["python","doSelections.py","-f",samp[1],"-zpt",cut[0],"-hpt",cut[1],"-met",cut[2],"-sdm","30.0","-b",cut[3],"-wp",cut[4],"-date",samp[0]])
                 subprocess.run(["python","doSelections.py","-f",samp[1],"-zpt",cut[0],"-hpt",cut[1],"-met",cut[2],"-sdm","30.0","-b",cut[3],"-wp",cut[4],"-date",samp[0],"-sr","True"])
-                #subprocess.run(["python","doSelections.py","-f",samp[1],"-zpt",cut[0],"-hpt",cut[1],"-met",cut[2],"-sdm","30.0","-b",cut[3],"-wp",cut[4],"-date",samp[0],"-c","True"])
+                subprocess.run(["python","doSelections.py","-f",samp[1],"-zpt",cut[0],"-hpt",cut[1],"-met",cut[2],"-sdm","30.0","-b",cut[3],"-wp",cut[4],"-date",samp[0],"-c","True"])
 
         for era in eras:
             print("   Beginning plottng and analysis for year {0}, with a luminosity of {1}".format(era[0],era[1]))
