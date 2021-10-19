94bbbc75aab2acfa99992464bbc35792305260a6
gracecummings <graceelainecummings@gmail.com>
2021-10-19 16:37:34 +0200

    added systematic for loop, still do not have the proper automation in the datacard writing, or in the background jec comp

--- makeDataCardsAndRootFiles.py
+++ makeDataCardsAndRootFiles.py
@@ -41,6 +41,9 @@ def writeDataCard(processes,rootFileName,channel):
         card.write(cardstr)
     #After here, put some sort of interation through a list of systematics
     card.close()
+
+#def gatherSystematics(confsecsyst):
+    
     
 
 if __name__=='__main__':
@@ -54,17 +57,16 @@ if __name__=='__main__':
     sigxs   = 1.0
     rebindiv = 2
 
-    #load in the files with the hists
-    bkgs        = go.backgrounds('BkgInputsNominalRebin/',zptcut,hptcut,metcut,btagwp)
-    systbkgsup  = go.backgrounds('BkgInputsJECSystRebin/',zptcut,hptcut,metcut,btagwp,"systjecup")
-    systbkgsdwn = go.backgrounds('BkgInputsJECSystRebin/',zptcut,hptcut,metcut,btagwp,"systjecdwn")
-    sig         = go.signal('SignalInputsNominalRebin/',zptcut,hptcut,metcut,btagwp,sigxs,101.27)
-    systsigup   = go.signal('SignalInputsJECSystRebin/',zptcut,hptcut,metcut,btagwp,sigxs,101.27,"systjecup")
-    systsigdwn  = go.signal('SignalInputsJECSystRebin/',zptcut,hptcut,metcut,btagwp,sigxs,101.27,"systjecdwn")
-    dyEst       = ROOT.TFile('BkgInputsNominalRebin/Run2_2017_2018_dy_extraploation_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.root')
-    dyEstup     = ROOT.TFile('BkgInputsJECSystRebin/Run2_2017_2018_dy_extraploationsystjecup_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.root')
-    dyEstdwn    = ROOT.TFile('BkgInputsJECSystRebin/Run2_2017_2018_dy_extraploationsystjecdwn_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.root')
+    config = configparser.RawConfigParser()
+    config.optionxform = str
+    fp = open('systematics.ini')
+    config.read_file(fp)
+    systs = config.sections()
 
+    ####load in the files with the nominal distributions
+    bkgs = go.backgrounds(config.get('btag','pathnom'),zptcut,hptcut,metcut,btagwp,config.get('btag','strnom'))
+    sig  = go.signal(config.get('btag','pathsignom'),zptcut,hptcut,metcut,btagwp,sigxs,101.27,config.get('btag','strnom'))
+    dyEst = ROOT.TFile(config.get('btag','pathnom')+'Run2_2017_2018_dy_extraploation'+config.get('btag','strnom')+'_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.root')
 
     ####Prepping holders####
     tf1 = ROOT.TFile(bkgs.f17dyjetsb[0])
@@ -73,57 +75,17 @@ if __name__=='__main__':
     empty1 = empty.Clone()
     empty2 = empty.Clone()
     empty3 = empty.Clone()
-    empty4 = empty.Clone()
-    empty5 = empty.Clone()
-    empty6 = empty.Clone()
-    empty7 = empty.Clone()
-    empty8 = empty.Clone()
-    empty9 = empty.Clone()
-
-    ####Gathering the Systematics####
-    httup  = systbkgsup.getAddedHist(empty4,"TT","sr","h_zp_jigm")
-    httdwn = systbkgsdwn.getAddedHist(empty5,"TT","sr","h_zp_jigm")
-    hzzup  = systbkgsup.getAddedHist(empty6,"ZZTo2L2Q","sr","h_zp_jigm")
-    hzzdwn = systbkgsdwn.getAddedHist(empty7,"ZZTo2L2Q","sr","h_zp_jigm")
-    hwzup  = systbkgsup.getAddedHist(empty8,"WZTo2L2Q","sr","h_zp_jigm")
-    hwzdwn = systbkgsdwn.getAddedHist(empty9,"WZTo2L2Q","sr","h_zp_jigm")
-    hvvup  = hzzup.Clone()
-    hvvdwn = hzzdwn.Clone()
-    hvvup.Add(hwzup)
-    hvvdwn.Add(hwzdwn)
-    hdyup  = dyEstup.Get("extrphist").Clone()
-    hdydwn = dyEstdwn.Get("extrphist").Clone()
-
-    #Rename and Restructure
-    httup.SetName("TT_jecUp")
-    httdwn.SetName("TT_jecDown")
-    hvvup.SetName("VV_jecUp")
-    hvvdwn.SetName("VV_jecDown")
-    hdyup.SetName("DY_jecUp")
-    hdydwn.SetName("DY_jecDown")
-    httup.Rebin(rebindiv)
-    httdwn.Rebin(rebindiv)
-    hvvup.Rebin(rebindiv)
-    hvvdwn.Rebin(rebindiv)
-    httup.GetXaxis().SetRangeUser(1500,5000)
-    httdwn.GetXaxis().SetRangeUser(1500,5000)
-    hvvup.GetXaxis().SetRangeUser(1500,5000)
-    hvvdwn.GetXaxis().SetRangeUser(1500,5000)
-    hdyup.GetXaxis().SetRangeUser(1500,5000)
-    hdydwn.GetXaxis().SetRangeUser(1500,5000)
     
     ####Getting the Estimations####
     hdat = empty.Clone()
+    hdy    = dyEst.Get("extrphist").Clone()
     htt = bkgs.getAddedHist(empty1,"TT","sr","h_zp_jigm")
     hzz  = bkgs.getAddedHist(empty2,"ZZTo2L2Q","sr","h_zp_jigm")
     hwz  = bkgs.getAddedHist(empty3,"WZTo2L2Q","sr","h_zp_jigm")
     hvv  = hzz.Clone()
     hvv.Add(hwz)
 
-    #Get DY from estimation
-    hdy    = dyEst.Get("extrphist").Clone()
-
-    #Rename and restructure background if necessary
+    ####Rename and restucture
     htt.SetName("TT")
     hvv.SetName("VV")
     hdy.SetName("DY")
@@ -135,28 +97,24 @@ if __name__=='__main__':
     hvv.GetXaxis().SetRangeUser(1500,5000)
     hdy.GetXaxis().SetRangeUser(1500,5000)
 
-    #Signal Samples
+    ####For Each signal, make a datacard, and a root file with all systematics
     siginfo = sig.prepsigsr
-    sigJECup = systsigup.prepsigsr
-    sigJECdwn = systsigdwn.prepsigsr
     sigcolors = go.colsFromPalette(siginfo,ROOT.kCMYK)
-
-    #Make a plot with all of the nominal signals plotted
-    #tcAllSig = ROOT.TCanvas("tcAllSig","AllSig",600,600)
-    #sigAllname = go.makeOutFile("allsig_2018_NS200",'nominaljec','.png',str(zptcut),str(hptcut),str(metcut),str(btagwp))
     for s,sig in enumerate(siginfo):
         name = sig["name"]
         signame = "holder"
-        if name != sigJECup[s]["name"] !=  sigJECdwn[s]["name"]:
-            print("Cannot find matching entries in JEC systematics collections, aborting")
-            break
         if "Tune" in name:
             strippedname = name.split("_Tune")[0]
             signame = strippedname.replace("-","")
         else:
             signame = name.replace("-","")
+        print("------- Looking at signal sample ",signame)
 
-        #nominal    
+        ####Make Files
+        prepRootName = go.makeOutFile('Run2_2017_2018_ZllHbbMET',chan+'_'+signame,'.root',str(zptcut),str(hptcut),str(metcut),str(btagwp))
+        prepRootFile = ROOT.TFile(prepRootName,"recreate")
+            
+        ####Nominal Signal
         hsigori = sig["tfile"].Get("h_zp_jigm")
         hsigori.Sumw2(ROOT.kTRUE)#Throws a warning that it is already created
         hsig = hsigori.Clone()
@@ -164,33 +122,125 @@ if __name__=='__main__':
         hsig.Scale(sig["scale"])
         hsig.Rebin(rebindiv)
         hsig.GetXaxis().SetRangeUser(1500,5000)
+        prepRootFile.cd() 
+        htt.Write()
+        hvv.Write()
+        hdy.Write()
+        hdat.Write()
+        hsig.Write()
 
-        #up
-        hsigupori = sigJECup[s]["tfile"].Get("h_zp_jigm")
-        hsigup = hsigupori.Clone()
-        hsigup.SetName(signame+"_jecUp")
-        hsigup.Scale(sigJECup[s]["scale"])
-        hsigup.Rebin(rebindiv)
-        hsigup.GetXaxis().SetRangeUser(1500,5000)
 
-        #down
-        hsigdwnori = sigJECdwn[s]["tfile"].Get("h_zp_jigm")
-        hsigdwn = hsigdwnori.Clone()
-        hsigdwn.SetName(signame+"_jecDown")
-        hsigdwn.Scale(sigJECdwn[s]["scale"])
-        hsigdwn.Rebin(rebindiv)
-        hsigdwn.GetXaxis().SetRangeUser(1500,5000)
-        
+        for syst in systs[1:]:
+            print("------- Looking at systematic ",syst)
+            
+            systbkgsup  = go.backgrounds(config.get(syst,'pathup'),zptcut,hptcut,metcut,btagwp,config.get(syst,'strup'))
+            systbkgsdwn = go.backgrounds(config.get(syst,'pathdwn'),zptcut,hptcut,metcut,btagwp,config.get(syst,'strdwn'))
+            systsigup   = go.signal(config.get(syst,'pathsigup'),zptcut,hptcut,metcut,btagwp,sigxs,101.27,config.get(syst,'strup'))
+            systsigdwn  = go.signal(config.get(syst,'pathsigdwn'),zptcut,hptcut,metcut,btagwp,sigxs,101.27,config.get(syst,'strdwn'))
 
-        #This is need to get the errors on the bins correct
-        #built for nominal case
-        #but not needed for Combine
-        #for ibin in range(hsig.GetNbinsX()+1):
-        # oribin = hsigori.GetBinContent(ibin)
-        # orierr = hsigori.GetBinError(ibin)
-        # newbinval = oribin*sig["scale"]
-        # hsig.SetBinContent(ibin,newbinval)
-        # hsig.SetBinError(ibin,newbinval**(1/2))
+            if rebindiv == 2:
+                dyEstup     = ROOT.TFile(config.get(syst,'pathup')+'Run2_2017_2018_dy_extraploation'+config.get(syst,'strup')+'_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.root')
+                dyEstdwn    = ROOT.TFile(config.get(syst,'pathdwn')+'Run2_2017_2018_dy_extraploation'+config.get(syst,'strdwn')+'_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.root')
+
+            ####Make it useful
+            sigup  = systsigup.prepsigsr
+            sigdwn = systsigdwn.prepsigsr
+            if name != sigup[s]["name"] !=  sigdwn[s]["name"]:
+                print("Cannot find matching entries in systematics collections, aborting")
+                break
+
+            ####Prepping holders####
+            empty4 = empty.Clone()
+            empty5 = empty.Clone()
+            empty6 = empty.Clone()
+            empty7 = empty.Clone()
+            empty8 = empty.Clone()
+            empty9 = empty.Clone()
+
+            ####Gathering the Systematic
+            #Background
+            httup  = systbkgsup.getAddedHist(empty4,"TT","sr","h_zp_jigm")
+            httdwn = systbkgsdwn.getAddedHist(empty5,"TT","sr","h_zp_jigm")
+            hzzup  = systbkgsup.getAddedHist(empty6,"ZZTo2L2Q","sr","h_zp_jigm")
+            hzzdwn = systbkgsdwn.getAddedHist(empty7,"ZZTo2L2Q","sr","h_zp_jigm")
+            hwzup  = systbkgsup.getAddedHist(empty8,"WZTo2L2Q","sr","h_zp_jigm")
+            hwzdwn = systbkgsdwn.getAddedHist(empty9,"WZTo2L2Q","sr","h_zp_jigm")
+            hvvup  = hzzup.Clone()
+            hvvdwn = hzzdwn.Clone()
+            hvvup.Add(hwzup)
+            hvvdwn.Add(hwzdwn)
+            hdyup  = dyEstup.Get("extrphist").Clone()
+            hdydwn = dyEstdwn.Get("extrphist").Clone()
+
+            #Rename and Restructure
+            httup.SetName("TT_"+syst+"Up")
+            httdwn.SetName("TT_"+syst+"Down")
+            hvvup.SetName("VV_"+syst+"Up")
+            hvvdwn.SetName("VV_"+syst+"Down")
+            hdyup.SetName("DY_"+syst+"Up")
+            hdydwn.SetName("DY_"+syst+"Down")
+            httup.Rebin(rebindiv)
+            httdwn.Rebin(rebindiv)
+            hvvup.Rebin(rebindiv)
+            hvvdwn.Rebin(rebindiv)
+            httup.GetXaxis().SetRangeUser(1500,5000)
+            httdwn.GetXaxis().SetRangeUser(1500,5000)
+            hvvup.GetXaxis().SetRangeUser(1500,5000)
+            hvvdwn.GetXaxis().SetRangeUser(1500,5000)
+            hdyup.GetXaxis().SetRangeUser(1500,5000)
+            hdydwn.GetXaxis().SetRangeUser(1500,5000)
+            
+            #Signal
+            hsigupori = sigup[s]["tfile"].Get("h_zp_jigm")
+            hsigup = hsigupori.Clone()
+            hsigup.SetName(signame+"_"+syst+"Up")
+            hsigup.Scale(sigup[s]["scale"])
+            hsigup.Rebin(rebindiv)
+            hsigup.GetXaxis().SetRangeUser(1500,5000)
+            
+            hsigdwnori = sigdwn[s]["tfile"].Get("h_zp_jigm")
+            hsigdwn = hsigdwnori.Clone()
+            hsigdwn.SetName(signame+"_"+syst+"Down")
+            hsigdwn.Scale(sigdwn[s]["scale"])
+            hsigdwn.Rebin(rebindiv)
+            hsigdwn.GetXaxis().SetRangeUser(1500,5000)
+
+            #Write the histograms
+            prepRootFile.cd()
+            print("------- Writing the up/dwn histograms")
+            hsigup.Write()
+            hsigdwn.Write()
+            httup.Write()
+            httdwn.Write()
+            hvvup.Write()
+            hvvdwn.Write()
+            hdyup.Write()
+            hdydwn.Write()
+
+            #This is need to get the errors on the bins correct
+            #built for nominal case
+            #but not needed for Combine
+            #for ibin in range(hsig.GetNbinsX()+1):
+            # oribin = hsigori.GetBinContent(ibin)
+            # orierr = hsigori.GetBinError(ibin)
+            # newbinval = oribin*sig["scale"]
+            # hsig.SetBinContent(ibin,newbinval)
+            # hsig.SetBinError(ibin,newbinval**(1/2))
+
+            ##### Make Plot of each syst for each signal sample
+            tc = ROOT.TCanvas("tc",signame,600,600)
+            hsig.SetLineColor(ROOT.kBlack)
+            hsigup.SetLineColor(ROOT.kRed)
+            hsigdwn.SetLineColor(ROOT.kBlue)
+            hsigup.SetStats(0)
+            hsigup.GetXaxis().SetTitle("Z' Jigsaw Mass Estmator")
+            tc.cd()
+            hsigup.Draw('hist')
+            hsig.Draw('histsame')
+            hsigdwn.Draw('histsame')
+            sigplotname = go.makeOutFile(signame,syst+'updowncomp','.png',str(zptcut),str(hptcut),str(metcut),str(btagwp))
+            tc.SaveAs(sigplotname)
+
 
         #For writing the datacard
         #it makes sense to have the line be the key,
@@ -205,65 +255,19 @@ if __name__=='__main__':
                             #{"type":"shape","unc":1,"proc":[0,1,0,0]},
                             "jec":
                             {"type":"shapeN2","unc":1,"proc":[1,1,1,1]},
-                            },
-                    }
-
-    
-        prepRootName = go.makeOutFile('Run2_2017_2018_ZllHbbMET',chan+'_'+signame,'.root',str(zptcut),str(hptcut),str(metcut),str(btagwp))
-        prepRootFile = ROOT.TFile(prepRootName,"recreate")
-        htt.Write()
-        hvv.Write()
-        hdy.Write()
-        hdat.Write()
-        hsig.Write()
-        hsigup.Write()
-        hsigdwn.Write()
-        httup.Write()
-        httdwn.Write()
-        hvvup.Write()
-        hvvdwn.Write()
-        hdyup.Write()
-        hdydwn.Write()
-
+                    },
+        }
+        print("------- Defined the Datacard Dict")
+        print("------- Writing the Datacard")
+        writeDataCard(procdict,prepRootName,chan)
+        print("------- About to close the root file")
         prepRootFile.Close()
 
-        writeDataCard(procdict,prepRootName,chan)
-
-        #Make the plot of all signal samples
-        #tcAllSig.cd()
-        #hsig.SetLineColor(sigcolors[s])
-        #hsig.SetStats(0)
-        #hsig.GetXaxis().SetTitle("Z' Jigsaw Mass Estimator")
-        #if s == 0:
-        #    hsig.Draw('hist')
-        #else:
-        #    hsig.Draw('histsame')
-        #tcAllSig.Update()
-        
-        #Make Plot for each signal sample
-        tc = ROOT.TCanvas("tc",signame,600,600)
-        hsig.SetLineColor(ROOT.kBlack)
-        hsigup.SetLineColor(ROOT.kRed)
-        hsigdwn.SetLineColor(ROOT.kBlue)
-        hsigup.SetStats(0)
-        hsigup.GetXaxis().SetTitle("Z' Jigsaw Mass Estmator")
-        tc.cd()
-        hsigup.Draw('hist')
-        hsig.Draw('histsame')
-        hsigdwn.Draw('histsame')
-        sigplotname = go.makeOutFile(signame,'jecupdowncomp','.png',str(zptcut),str(hptcut),str(metcut),str(btagwp))
-        tc.SaveAs(sigplotname)
-        #tcAllSig.cd()
-
-    #SAve the total plot
-    #tcAllSig.SaveAs(sigAllname)
-
     #Debug for background
-
     #Extra plots
-    dynormup = np.load('BkgJECSyst/Run2_2017_2018_dynormalization_systjecup_signalblind_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.npy')[0]
-    dynormnom = np.load('BkgInputs/Run2_2017_2018_dynormalization_signalblind_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.npy')[0]
-    dynormdwn = np.load('BkgJECSyst/Run2_2017_2018_dynormalization_systjecdwn_signalblind_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.npy')[0]
+    dynormup = np.load(config.get('btag','pathup')+'Run2_2017_2018_dynormalization_'+config.get('btag','strup')+'_signalblind_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.npy')[0]
+    dynormnom = np.load(config.get('btag','pathnom')+'Run2_2017_2018_dynormalization_'+config.get('btag','strnom')+'_signalblind_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.npy')[0]
+    dynormdwn = np.load(config.get('btag','pathdwn')+'Run2_2017_2018_dynormalization_'+config.get('btag','strdwn')+'_signalblind_Zptcut150.0_Hptcut300.0_metcut200.0_btagwp0.8.npy')[0]
 
     print("DY norm up:  ",dynormup)
     print("DY norm nom: ",dynormnom)
@@ -483,7 +487,7 @@ if __name__=='__main__':
     tcbkg.cd()
     tcbkg.Update()
 
-    bkgjeccomp = go.makeOutFile('Run2_2017_2018_ZllHbbMET',chan+'_bkg_jeccomp','.png',str(zptcut),str(hptcut),str(metcut),str(btagwp))
+    bkgjeccomp = go.makeOutFile('Run2_2017_2018_ZllHbbMET',chan+'_bkg_badbtag','.png',str(zptcut),str(hptcut),str(metcut),str(btagwp))
     tcbkg.SaveAs(bkgjeccomp)
     
         
