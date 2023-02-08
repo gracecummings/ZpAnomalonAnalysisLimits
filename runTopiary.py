a864e19f3740dffb958b4e8c06011750709d27d7
gracecummings <graceelainecummings@gmail.com>
2023-02-08 11:36:50 -0600

    addded better book keeping to signal samples to fix a trimmer bug

--- runTopiary.py
+++ runTopiary.py
@@ -1,3 +1,4 @@
+
 import ROOT
 import glob
 import argparse
@@ -62,6 +63,12 @@ if __name__=="__main__":
         print("You have a problem, no channel given")
         sys.exit()
     origevnts = 0
+    trimmedevents = 0
+    passedfilter = 0
+    passedzcandnum = 0
+    passedfat = 0
+    passedfilandfat = 0
+    passedfilandz = 0
 
     #First set of print statements
     print( "Making topiary of ",samp)
@@ -76,22 +83,35 @@ if __name__=="__main__":
         #for non-signal samples
         inChain = ROOT.TChain("PreSelection")
         #inputs  = glob.glob("../dataHandling/"+year+"/"+samp+"*.root")
-        inputs  = glob.glob("../dataHandling/"+year+"_new/"+samp+"*.root")
-        #inputs  = glob.glob("../dataHandling/"+year+"_old/"+samp+"*.root")
+        #inputs  = glob.glob("../dataHandling/"+year+"_new/"+samp+"*.root")
+        #inputs  = glob.glob("../dataHandling/"+year+"_newee/"+samp+"*.root")
+        #inputs  = glob.glob("../dataHandling/"+year+"_newee/Autumn18_DYJetsToLL_2022-06-17/"+samp+"*.root")o
+        inputs  = glob.glob("../dataHandling/"+year+"_old/"+samp+"*.root")
         for f in inputs:
             #print(f)
             inChain.Add(f)
             tf = ROOT.TFile.Open(f)
             origevnts += tf.Get("hnevents").GetBinContent(1)
+            #trimmedevents += tf.Get("hpassall").GetBinContent(1)
+            #passedfilter += tf.Get("hpassfilter").GetBinContent(1)
+            #passedzcandnum += tf.Get("hpassZ").GetBinContent(1)
+            #passedfat += tf.Get("hpassfat").GetBinContent(1)
+            #passedfilandfat += tf.Get("hpassfilandfat").GetBinContent(1)
+            #passedfilandz += tf.Get("hpassfilandz").GetBinContent(1)
     elif ".root" in samp:
         #for debug, and ntuple in working directory
         inChain = ROOT.TChain("TreeMaker2/PreSelection")
         inChain.Add(samp)
     elif samptype == 1 and ".root" not in samp:
-        inChain = ROOT.TChain("TreeMaker2/PreSelection")
-        #inputs  = glob.glob("../dataHandling/"+year+"/"+samp+"*.root")
-        inputs  = glob.glob("../dataHandling/"+year+"_old/"+samp+"*.root")
-        print(inputs)
+       # inChain = ROOT.TChain("TreeMaker2/PreSelection")
+        inChain = ROOT.TChain("PreSelection")
+        if "2018" not in year:
+            inputs  = glob.glob("../dataHandling/"+year+"_newee/"+samp+"*.root")
+        else:
+            inputs  = glob.glob("../dataHandling/"+year+"_old/"+samp+"*.root")
+        #inputs   = glob.glob("../dataHandling/2018_data_muonunc/Autumn18_ZpAnomalonHZ_2023-01-26/"+samp+"*.root")
+        #
+        #print(inputs)
         for f in inputs:
             inChain.Add(f)
             tf = ROOT.TFile.Open(f)
@@ -137,24 +157,35 @@ if __name__=="__main__":
         print("Too many systematic flags at one time, stopping this maddness")
         exit()
 
+
+    if (origevnts < 1):
+        print('      ***Could not get events from the trims, getting Processed events from name')
+        checkevnts = 0
+        for f in inputs:
+            nproc = int((f.split("MiniAOD")[0]).split('skim_')[-1])
+            checkevnts +=nproc
+        origevnts = checkevnts
         
     #outFile = go.makeOutFile(samp,'topiary_'+args.channel+'_'+syststring,'.root','No','Req','On','Reco')#Needs to become dynamic with cuts
-    #outFile = go.makeOutFile(samp,'topiary_'+args.channel+'_'+syststring,'.root','0.0','250.0','0.0','0.0')#normal name
-    outFile = go.makeOutFile(samp,'topiary_'+args.channel+'_'+syststring,'.root','0.0','0.0','0.0','0.0')#nemu
-
+    outFile = go.makeOutFile(samp,'topiary_'+args.channel+'_'+syststring,'.root','0.0','250.0','0.0','0.0')#normal name
+    #outFile = go.makeOutFile(samp,'topiary_'+args.channel+'_'+syststring,'.root','0.0','0.0','0.0','0.0')#nemu
     print("     Systematics ",syststring)
     print("     Events in TChain: ",inChain.GetEntries())
+    print("     Number of files in the TChain: ",len(inputs))
     print(("     Original data set had {0} events in type.").format(origevnts))
     print("    Saving topiary in ",outFile)
+    #print("    trimmed events ",trimmedevents)
+    #print("    passed the filters ",passedfilter)
+    #print("    passed the Z number ",passedzcandnum)
+    #print("    passed the fat number ",passedfat)
+    #print("    passed filters and fat ",passedfilandfat)
+    #print("    passed filters and Z ",passedfilandz)
+
 
     ROOT.gSystem.Load("../UHH2/JetMETObjects/obj/libSUHH2JetMETObjects.so")
     ROOT.gROOT.ProcessLine(".include ../UHH2/JetMETObjects/interface")
     ROOT.gSystem.Load("TreeMakerTopiary.so")
     ROOT.gInterpreter.Declare('#include "TreeMakerTopiary.h"')
-
-
-    #topiary = ROOT.TreeMakerTopiary(inChain,samptype,topyear,channel,systind)
-    #topiary.Loop(outFile,origevnts,samptype,topyear,channel,systind)
 
     topiary = ROOT.TreeMakerTopiary(inChain,samptype,topyear,channel,sysvec)
     topiary.Loop(outFile,origevnts,samptype,topyear,channel,sysvec)
