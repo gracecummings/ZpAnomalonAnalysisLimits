3472767972882cad8142193cfef41eccc4b8b161
GraceCummings <cummingsge@vcu.edu>
2023-09-26 16:34:20 -0500

    updated job scripts to actually run restframes in batch

---
+++ condorbatch/submitTopiaryJobs.py
@@ -0,0 +1,79 @@
+import json
+import sys
+import os
+import argparse
+from datetime import date
+
+#def getSampleName(sampstr):
+#    name = "badsamp"
+#    if "WZ" in sampstr:
+#        name = sampstr.split(")#
+
+parser = argparse.ArgumentParser()
+
+if __name__=='__main__':
+    parser.add_argument("-j","--sampleJson",help=".json file with the skims to be made into topiary")
+    parser.add_argument("-s","--sample",type=str,help="specific sample you want make a topiary of")
+    parser.add_argument("-c","--channel",type=str,help="channel: mumu,ee,emu")
+    parser.add_argument("-syst","--syststr",type=str,default="",help="the systematic flag you want topiary to make")
+    args = parser.parse_args()
+
+    #Check json
+    if (args.sampleJson is None):
+        print("No list of skims provided, please provide appropriate json")
+        fsjson = {}
+    else:
+        if (not os.path.exists(args.sampleJson)):
+            print("Invalid json")
+        fstotop = open(args.sampleJson,'r')
+        fsjson = json.load(fstotop)
+
+    #Make log directories
+    if not os.path.exists("condorMonitoringOutput/"+str(date.today())+"/"):
+        os.makedirs("condorMonitoringOutput/"+str(date.today())+"/")
+
+    #Tar the working area
+    print("Creating tarball of working area")
+    tarballName = "topiaryForCondor.tar.gz"
+    os.system("tar -hcf "+tarballName+" topiary_jobs")
+
+
+    #Submit the jobs
+    if len(fsjson.keys()) > 0:
+        for key in fsjson.keys():
+            for samp in fsjson[key]:
+                if args.sample:
+                    if args.sample not in samp:
+                        continue
+                #print(fsjson[key][samp])
+                sampleName = samp.split("_13TeV")[0]
+                #print("    ",sampleName)
+                fullpaths = ["root://cmseos.fnal.gov/"+key+"/"+x for x in fsjson[key][samp]]
+                samplistasstr = str(fullpaths).replace(' ','')
+                #print(samplistasstr)
+
+                #Where do you want to save it
+                eosForOutput = "root://cmseos.fnal.gov//store/user/lpcboostres/"+sampleName+"_"+str(date.today())
+                eosOnlypath  = eosForOutput.split("root://cmseos.fnal.gov/")[-1]
+                os.system("eos root://cmseos.fnal.gov mkdir {}".format(eosOnlypath))
+                print("Topiaries from {0} are being written to {1}".format(sampleName,eosOnlypath))
+
+                #Make the jdl for each sample
+                jdlName = "topiary_"+sampleName+"_"+str(date.today())+".jdl"
+                jdl = open(jdlName,"w")
+                jdl.write("universe = vanilla\n")
+                jdl.write("Should_Transfer_Files = YES\n")
+                jdl.write("WhenToTransferOutput = ON_EXIT\n")
+                jdl.write("Transfer_Input_Files = "+tarballName+"\n")
+                jdl.write("Output = condorMonitoringOutput/{0}/{1}_out.stdout\n".format(str(date.today()),sampleName))
+                jdl.write("Error = condorMonitoringOutput/{0}/{1}_err.stder\n".format(str(date.today()),sampleName))
+                jdl.write("Log = condorMonitoringOutput/{0}/{1}_log.log\n".format(str(date.today()),sampleName))
+                jdl.write("Executable = topiary.sh\n")
+                jdl.write("Arguments = {0} {1} {2} {3} {4}\n".format(eosForOutput,sampleName,samplistasstr,args.channel,args.syststr))
+                jdl.write('+DESIRED_Sites="T3_US_Baylor,T2_US_Caltech,T3_US_Colorado,T3_US_Cornell,T3_US_FIT,T3_US_FNALLPC,T3_US_Omaha,T3_US_JHU,T3_US_Kansas,T2_US_MIT,T3_US_NotreDame,T2_US_Nebraska,T3_US_NU,T3_US_OSU,T3_US_Princeton_ICSE,T2_US_Purdue,T3_US_Rice,T3_US_Rutgers,T3_US_MIT,T3_US_NERSC,T3_US_SDSC,T3_US_FIU,T3_US_FSU,T3_US_OSG,T3_US_TAMU,T3_US_TTU,T3_US_UCD,T3_US_UCSB,T2_US_UCSD,T3_US_UMD,T3_US_UMiss,T2_US_Vanderbilt,T2_US_Wisconsin"')
+                jdl.write("\n")
+                jdl.write("Queue 1\n")#Not sure about this one
+                jdl.close()
+                
+                #submit the jobs
+                os.system("condor_submit {0}".format(jdlName))
---
+++ condorbatch/topiary.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#untar your crap
+echo "Untaring  directory with topiary code"
+tar -xf topiaryForCondor.tar.gz
+cd topiary_jobs
+
+
+#source the environment
+echo "Sourcing the environment"
+source /cvmfs/sft.cern.ch/lcg/views/LCG_99/x86_64-centos7-gcc10-opt/setup.sh
+source setup_RestFrames.sh
+echo ${RESTFRAMESSYS}
+
+#Arguments taken
+#1 - the eos directory to drop the topiary
+#2 - the sample name
+#3 - the files to put into the topiary
+#4 - the channel 
+#5 - the systematic selection
+
+#Running topiary 
+echo "Making topiary"
+python runTopiary.py -s $2 -c $4 -l $3 $5
+
+for FILE in ./*.root
+do
+    echo "copying ${FILE} to eos $1"
+    xrdcp ${FILE} $1/${FILE}
+    XRDEXIT=$?
+    if [[ $XRDEXIT -ne 0 ]]; then
+	rm ${FILE}
+	echo "failure in xrdcp, exit code $XRDEXIT"
+	exit $XRDEXIT
+    fi
+    rm ${FILE}
+done
+
+
+#cleanup
+cd ${_CONDOR_SCRATCH_DIR_}
+rm -rf topiary_jobs
+rm topiaryForCondor.tar.gz
